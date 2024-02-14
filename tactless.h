0a64bab2f78218e19436b0c384206b9af11b8095
Ferronn <ferronn@ferronn.dev>
2024-02-13 21:28:06 -0600

    TACTLESS_EXPORT

--- tactless.h
+++ tactless.h
@@ -1,11 +1,13 @@
 #ifndef __TACTLESS_H__
 #define __TACTLESS_H__
 
+#include "tactless_export.h"
+
 struct tactless;
 typedef struct tactless tactless;
 
-tactless *tactless_open(const char *product);
-void tactless_close(tactless *t);
-void tactless_dump(const tactless *t);
+TACTLESS_EXPORT tactless *tactless_open(const char *product);
+TACTLESS_EXPORT void tactless_close(tactless *t);
+TACTLESS_EXPORT void tactless_dump(const tactless *t);
 
 #endif
