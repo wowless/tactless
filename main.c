f39581c802a1c22337708db4cc72d527fa725f54
Ferronn <ferronn@ferronn.dev>
2025-01-28 16:43:57 +0000

    main: tableify commands

--- main.c
+++ main.c
@@ -37,87 +37,135 @@ static int dump_archive_index(const unsigned char *text, size_t size) {
   return 1;
 }
 
-static int dump(const char *type, const char *filename) {
-  size_t size;
-  unsigned char *text = tactless_readfile(filename, &size);
-  if (!text) {
+struct dump_command {
+  const char *name;
+  int (*fn)(const unsigned char *, size_t);
+};
+
+const struct dump_command dump_commands[] = {
+    {"root", dump_root},
+    {"encoding", dump_encoding},
+    {"archive_index", dump_archive_index},
+    {0, 0},
+};
+
+static int dump(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("usage: tactless dump $format $filename\n", stderr);
     return 0;
   }
-  int ret = 1;
-  if (strcmp(type, "root") == 0) {
-    ret = dump_root(text, size);
-  } else if (strcmp(type, "encoding") == 0) {
-    ret = dump_encoding(text, size);
-  } else if (strcmp(type, "archive_index") == 0) {
-    ret = dump_archive_index(text, size);
-  } else {
-    fputs("invalid type\n", stderr);
-    ret = 0;
+  size_t size;
+  unsigned char *text = tactless_readfile(argv[1], &size);
+  if (!text) {
+    fputs("error reading file\n", stderr);
+    return 0;
   }
+  for (const struct dump_command *c = dump_commands; c->name; ++c) {
+    if (strcmp(argv[0], c->name) == 0) {
+      int ret = c->fn(text, size);
+      free(text);
+      return ret;
+    }
+  }
+  fputs("invalid type\n", stderr);
   free(text);
+  return 0;
+}
+
+static int fdid(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("usage: tactless fdid $product $fdid\n", stderr);
+    return 0;
+  }
+  tactless *t = tactless_open(argv[0], 0);
+  if (!t) {
+    fputs("error opening tactless\n", stderr);
+    return 0;
+  }
+  size_t size;
+  unsigned char *data = tactless_get_fdid(t, atoi(argv[1]), &size);
+  if (!data) {
+    tactless_close(t);
+    return 0;
+  }
+  int ret = fwrite(data, size, 1, stdout) == size;
+  tactless_close(t);
   return ret;
 }
 
-int main(int argc, char **argv) {
-  if (argc == 4 && strcmp(argv[1], "dump") == 0) {
-    return dump(argv[2], argv[3]) ? EXIT_SUCCESS : EXIT_FAILURE;
+static int name(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("usage: tactless name $product $name\n", stderr);
+    return 0;
   }
-  if (argc == 4 && strcmp(argv[1], "fdid") == 0) {
-    tactless *t = tactless_open(argv[2], 0);
-    if (!t) {
-      fputs("error opening tactless\n", stderr);
-      return EXIT_FAILURE;
-    }
-    size_t size;
-    unsigned char *data = tactless_get_fdid(t, atoi(argv[3]), &size);
-    if (!data) {
-      tactless_close(t);
-      return EXIT_FAILURE;
-    }
-    int ret =
-        fwrite(data, size, 1, stdout) == size ? EXIT_SUCCESS : EXIT_FAILURE;
-    tactless_close(t);
-    return ret;
-  }
-  if (argc == 4 && strcmp(argv[1], "name") == 0) {
-    tactless *t = tactless_open(argv[2], 0);
-    if (!t) {
-      fputs("error opening tactless\n", stderr);
-      return EXIT_FAILURE;
-    }
-    size_t size;
-    unsigned char *data = tactless_get_name(t, argv[3], &size);
-    if (!data) {
-      tactless_close(t);
-      return EXIT_FAILURE;
-    }
-    int ret =
-        fwrite(data, size, 1, stdout) == size ? EXIT_SUCCESS : EXIT_FAILURE;
-    tactless_close(t);
-    return ret;
-  }
-  if (argc == 3 && strcmp(argv[1], "build") == 0) {
-    char build[33];
-    if (!tactless_current_build(argv[2], build)) {
-      return EXIT_FAILURE;
-    }
-    puts(build);
-    return EXIT_SUCCESS;
-  }
-  if (argc != 2 && argc != 3) {
-    fputs("usage: tactless product\n", stderr);
-    return EXIT_FAILURE;
-  }
-  const char *build_config = 0;
-  if (argc == 3) {
-    build_config = argv[2];
-  }
-  tactless *t = tactless_open(argv[1], build_config);
+  tactless *t = tactless_open(argv[0], 0);
   if (!t) {
     fputs("error opening tactless\n", stderr);
-    return EXIT_FAILURE;
+    return 0;
+  }
+  size_t size;
+  unsigned char *data = tactless_get_name(t, argv[1], &size);
+  if (!data) {
+    tactless_close(t);
+    return 0;
+  }
+  int ret = fwrite(data, size, 1, stdout) == size;
+  tactless_close(t);
+  return ret;
+}
+
+static int build(int argc, char **argv) {
+  if (argc != 1) {
+    fputs("usage: tactless build $product\n", stderr);
+    return 0;
+  }
+  char build[33];
+  if (!tactless_current_build(argv[0], build)) {
+    return 0;
+  }
+  puts(build);
+  return 1;
+}
+
+static int summary(int argc, char **argv) {
+  if (argc < 1 || argc > 2) {
+    fputs("usage: tactless summary $product [$hash]\n", stderr);
+    return 0;
+  }
+  const char *build_config = 0;
+  if (argc == 2) {
+    build_config = argv[1];
+  }
+  tactless *t = tactless_open(argv[0], build_config);
+  if (!t) {
+    fputs("error opening tactless\n", stderr);
+    return 0;
   }
   tactless_dump(t);
   tactless_close(t);
-  return EXIT_SUCCESS;
+  return 1;
+}
+
+struct command {
+  const char *name;
+  int (*fn)(int, char **);
+};
+
+const struct command commands[] = {
+    {"build", build}, {"dump", dump},       {"fdid", fdid},
+    {"name", name},   {"summary", summary}, {0, 0},
+};
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    fputs("usage: tactless command ...\n", stderr);
+    return EXIT_FAILURE;
+  }
+  for (const struct command *c = commands; c->name; ++c) {
+    if (strcmp(argv[1], c->name) == 0) {
+      return c->fn(argc - 2, argv + 2) ? EXIT_SUCCESS : EXIT_FAILURE;
+    }
+  }
+  fputs("unknown command\n", stderr);
+  return EXIT_FAILURE;
 }
